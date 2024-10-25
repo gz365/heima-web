f85b6ed230ba36c651cfb93da0aad6f2eeac5925
高峥 <gaoz-a@glodon.com>
2024-10-25 10:34:41 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import LiveCamera from './components/LiveCamera.vue'
+import { ref } from 'vue'
+
+const text_arr_ref = ref([])
+let step = 1
+const interval = setInterval(() => {
+  text_arr_ref.value = [...text_arr_ref.value, '步骤' + step]
+  step++
+  if (step >= 5) {
+    clearInterval(interval)
+  }
+}, 1000)
+</script>
+
+<template>
+  <div class="app">
+    <LiveCamera />
+    <div>
+      <button>分析安全带佩戴情况</button>
+
+      <div>
+        <p v-for="(item, index) in text_arr_ref" :key="index">
+          {{ item }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  display: grid;
+  grid-template-columns: 700px auto;
+}
+</style>
---
+++ src/components/LiveCamera.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="live-container">
+    <iframe
+      src="https://aecloud-test.glodon.com/ivs/api/url/redirect/67124d03e1e2fc001dbb8e96/web"
+    ></iframe>
+
+    <iframe
+      src="https://aecloud-test.glodon.com/ivs/api/url/redirect/67124d03e1e2fc001dbb8e96/web"
+    ></iframe>
+  </div>
+</template>
+<style lang="scss">
+.live-container {
+  background-color: #eee;
+  width: 640px;
+  iframe {
+    display: block;
+    border: none;
+    width: 100%;
+    height: 360px;
+    &:not(:nth-child(1)) {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
